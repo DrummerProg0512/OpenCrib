e197a5ebf5a600332c5aae26150359e8eb285a89
Cesar Gallardo <kaesar_gallardo@hotmail.com>
2026-07-26 11:24:36 -0600

    Added UsersAreas Requests

---
+++ OpenCrib.Models/Requests/UsersAreasSelectRequest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace OpenCrib.Models.Requests
+{
+    public sealed class UsersAreasSelectRequest
+    {
+        [Range(1, int.MaxValue, ErrorMessage = "Users Areas ID must be greater than 0.")]
+        public int? UsersAreasID { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Area Location ID must be greater than 0.")]
+        public int? AreaLocationID { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "User ID must be greater than 0.")]
+        public int? UserID { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Updated By must be greater than 0.")]
+        public int? UpdatedBy { get; set; }
+
+        public DateTime? UpdatedOnStartDate { get; set; }
+
+        public DateTime? UpdatedOnEndDate { get; set; }
+
+        public bool? UsersAreaActive { get; set; }
+    }
+}
---
+++ OpenCrib.Models/Requests/UsersAreasUpdateRequest.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OpenCrib.Models.Requests
+{
+    public sealed class UsersAreasUpdateRequest
+    {
+        [Required]
+        [Range(1, int.MaxValue)]
+        public int UsersAreasID { get; set; }
+
+        [Range(1, int.MaxValue)]
+        public int? AreaLocationID { get; set; }
+
+        [Range(1, int.MaxValue)]
+        public int? UserID { get; set; }
+
+        [StringLength(27)]
+        public string? UpdatedOnString { get; set; }
+
+        [Range(1, int.MaxValue)]
+        public int? UpdatedBy { get; set; }
+
+        public bool? UsersAreaActive { get; set; }
+    }
+}
