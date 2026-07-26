cee8021aa028cb4f4089e4139e7c60fd005b369e
Cesar Gallardo <kaesar_gallardo@hotmail.com>
2026-07-26 16:24:53 -0600

    Added UserTypes DTO, Requests and Responses

---
+++ OpenCrib.Models/Requests/UserTypesBaseRequest.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OpenCrib.Models.Requests
+{
+    public abstract class UserTypesBaseRequest
+    {
+        [Required(ErrorMessage = "User type name is required.")]
+        [StringLength(255, ErrorMessage = "User type name cannot exceed 255 characters.")]
+        public string UserTypeName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Cost approval level is required.")]
+        [Range(1, int.MaxValue, ErrorMessage = "Cost approval level must be greater than 0.")]
+        public int CostApprovalLevel { get; set; }
+
+        [Required(ErrorMessage = "Cost approval amount is required.")]
+        [Range(typeof(decimal), "0", "999999999999999999", ErrorMessage = "Cost approval amount must be non-negative.")]
+        public decimal CostApprovalAmount { get; set; }
+
+        public bool IsActive { get; set; } = true;
+    }
+}
---
+++ OpenCrib.Models/Responses/UserTypesInsertResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace OpenCrib.Models.Responses
+{
+    public sealed class UserTypesInsertResponse : BaseResponse
+    {
+        public int UserTypeID { get; set; }
+    }
+}
