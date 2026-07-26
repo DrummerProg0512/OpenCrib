fc65243224ad80e81c3d61b02f3ab6c05fe9f709
Cesar Gallardo <kaesar_gallardo@hotmail.com>
2026-07-26 11:42:32 -0600

    Added UsersAreas DTOs and Responses

---
+++ OpenCrib.Models/DTOs/UsersAreas.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OpenCrib.Models.DTOs
+{
+    public sealed class UsersAreas
+    {
+        public int UsersAreasID { get; set; }
+        public int AreaLocationID { get; set; }
+        public int AreaLocationIDRef { get; set; }
+        public string? AreaLocationName { get; set; }
+        public int UserID { get; set; }
+        public int UserIDRef { get; set; }
+        public string? UserName { get; set; }
+        public string? UserLastName { get; set; }
+        public DateTime UpdatedOn { get; set; }
+        public int UpdatedBy { get; set; }
+        public int UpdatedByUserIDRef { get; set; }
+        public string? UpdatedByUserName { get; set; }
+        public bool UsersAreaActive { get; set; }
+    }
+}
---
+++ OpenCrib.Models/Responses/UsersAreasSelectResponse.cs
@@ -0,0 +1,9 @@
+﻿using OpenCrib.Models.DTOs;
+
+namespace OpenCrib.Models.Responses
+{
+    public sealed class UsersAreasSelectResponse : BaseResponse
+    {
+        public List<UsersAreas> UsersAreasResult { get; set; } = new List<UsersAreas>();
+    }
+}
