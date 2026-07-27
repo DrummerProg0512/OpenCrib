11915fef93c964990b5d839ba54782de12b09fa2
Cesar Gallardo <kaesar_gallardo@hotmail.com>
2026-07-26 19:05:57 -0600

    Added Services project and Connection strings

---
+++ OpenCrib.Services/Connections/Conn.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenCrib.Services.Connections
+{
+    public static class Conn
+    {
+        public static string OpenCribDB = "Server=WIN-OTIGVM1EOCB; Database=OpenCrib.DB; User Id=OpenCribUserOpenCribUser; Password=qMAfRjZKaysWhJT6wxzn; TrustServerCertificate=True;";
+    }
+}
