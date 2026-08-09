24c66f7308bdae59ab0bb2d1c1c9db27470eb85a
Cesar Gallardo <kaesar_gallardo@hotmail.com>
2026-08-08 20:28:27 -0600

    Updated CRUD_AreaLocations

--- OpenCrib.Services/CRUD/CRUD_AreaLocations.cs
+++ OpenCrib.Services/CRUD/CRUD_AreaLocations.cs
@@ -22,6 +22,7 @@ namespace OpenCrib.Services.CRUD
             _mapper = mapper;
         }
 
+        #region Create
         public async Task<AreaLocationInsertResponse> AreaLocationInsert(AreaLocationInsertRequest request)
         {
             var response = new AreaLocationInsertResponse();
@@ -94,5 +95,63 @@ namespace OpenCrib.Services.CRUD
             }
             return response;
         }
+        #endregion
+
+        #region Retrieve
+        public async Task<AreaLocationSelectResponse> AreaLocationSearch(AreaLocationSelectRequest request)
+        {
+            var response = new AreaLocationSelectResponse();
+            try
+            {
+                var parameters = new DynamicParameters();
+                parameters.Add("@AreaLocationID", request.AreaLocationID, DbType.Int32);
+                parameters.Add("@AreaLocationTypeID", request.AreaLocationTypeID, DbType.Int32);
+                parameters.Add("@AreaLocationName", request.AreaLocationName, DbType.String);
+                parameters.Add("@AreaLocationCode", request.AreaLocationCode, DbType.String);
+                parameters.Add("@AreaLocationActive", request.AreaLocationActive, DbType.Boolean);
+                parameters.Add("@UpdatedBy", request.UserID, DbType.Int32);
+                parameters.Add("@UpdatedOnStartDate", request.UpdatedOnStart?.ToString("yyyy-MM-dd HH:mm:ss"), DbType.String);
+                parameters.Add("@UpdatedOnEndDate", request.UpdatedOnEnd?.ToString("yyyy-MM-dd HH:mm:ss"), DbType.String);
+                using (var conn = new SqlConnection(Connections.Conn.OpenCribDB))
+                {
+                    await conn.OpenAsync();
+                    var result = await conn.QueryAsync<AreaLocation>("[dbo].[usp_AreaLocations_Select]", parameters, commandType: CommandType.StoredProcedure);
+                    response.Locations = result.AsList();
+                    response.IsSuccessful = true;
+                }
+            }
+            catch (SqlException ex)
+            {
+                response.IsSuccessful = false;
+                response.exMessage = $"SQL Error: {ex.Message}";
+            }
+            catch (TimeoutException ex)
+            {
+                response.IsSuccessful = false;
+                response.exMessage = $"Timeout Error: {ex.Message}";
+            }
+            catch (InvalidOperationException ex)
+            {
+                response.IsSuccessful = false;
+                response.exMessage = $"Invalid Operation: {ex.Message}";
+            }
+            catch (DbException ex)
+            {
+                response.IsSuccessful = false;
+                response.exMessage = $"Database Error: {ex.Message}";
+            }
+            catch (Exception ex)
+            {
+                response.IsSuccessful = false;
+                response.exMessage = $"General Error: {ex.Message}";
+            }
+            return response;
+        }
+        #endregion
+
+        #region Update
+
+        #endregion
+
     }
 }
