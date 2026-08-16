f198c254ca115ec919e623b411ca1142b15d775e
Cesar Gallardo <kaesar_gallardo@hotmail.com>
2026-08-16 16:53:06 -0600

    Added PartAssetAttachment map profile

---
+++ OpenCrib.Models/MappingProfiles/PartAssetAttachmentMapProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using OpenCrib.Models.Requests;
+using OpenCrib.Models.Responses;
+
+namespace OpenCrib.Models.MappingProfiles
+{
+    public sealed class PartAssetAttachmentMapProfile : Profile
+    {
+        public PartAssetAttachmentMapProfile()
+        {
+            CreateMap<PartAssetsAttachmentInsertRequest, PartAssetsAttachmentInsertResponse>()
+                .ForMember(dest => dest.IsSuccessful, opt => opt.Ignore())
+                .ForMember(dest => dest.exMessage, opt => opt.Ignore())
+                .ForMember(dest => dest.NewPartAssetsAttachmentID, opt => opt.Ignore())
+                .ForMember(dest => dest.OriginalRequest, opt => opt.MapFrom(src => src));
+        }
+    }
+}
