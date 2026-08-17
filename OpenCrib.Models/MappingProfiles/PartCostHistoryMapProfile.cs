ebb94fe617350c51d98ef42d7a7bf19376e4cc38
Cesar Gallardo <kaesar_gallardo@hotmail.com>
2026-08-16 18:05:37 -0600

    Added PartCostHistory map profile

---
+++ OpenCrib.Models/MappingProfiles/PartCostHistoryMapProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using OpenCrib.Models.Requests;
+using OpenCrib.Models.Responses;
+
+namespace OpenCrib.Models.MappingProfiles
+{
+    public sealed class PartCostHistoryMapProfile : Profile
+    {
+        public PartCostHistoryMapProfile()
+        {
+            CreateMap<PartCostHistoryInsertRequest, PartCostHistoryInsertResponse>()
+                .ForMember(dest => dest.IsSuccessful, opt => opt.Ignore())
+                .ForMember(dest => dest.exMessage, opt => opt.Ignore())
+                .ForMember(dest => dest.NewPartCostHistoryID, opt => opt.Ignore())
+                .ForMember(dest => dest.OriginalRequest, opt => opt.MapFrom(src => src));
+        }
+    }
+}
